b8135cc8e811b905c77015dd62f631dc23761574
Evgeny Kalashnikov <lumyfly@gmail.com>
2017-11-02 22:05:47 +0200

    exclude

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
             name: "GithubSDK",
             targets: ["GithubSDK"]),
     ],
+    exclude: ["Sources/GithubSDK/graphqls", "README.md", "schema.json", "proj_config.xcconfig", "pkg", "Demo", "apollo_gen.sh", "add-apollo-codegen-script.rb"],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -25,6 +26,5 @@ let package = Package(
         .testTarget(
             name: "GithubSDKTests",
             dependencies: ["GithubSDK","Apollo"]),
-    ],
-    exclude: ["Sources/GithubSDK/graphqls", "README.md", "schema.json", "proj_config.xcconfig", "pkg", "Demo", "apollo_gen.sh", "add-apollo-codegen-script.rb"]
+    ]
 )
